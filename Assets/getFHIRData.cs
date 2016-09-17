bbdf427f2370ae39bc876e185c708963a7204ad8
Askabe <dawit.askabe@dsiglobal.com>
2016-09-17 14:58:13 -0500

    init

---
+++ Assets/getFHIRData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatientDataDownload : MonoBehaviour {
+
+
+    public Vector3 newVertice;
+    public Vector2 newUV;
+    public int newTrianglee;
+
+
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
